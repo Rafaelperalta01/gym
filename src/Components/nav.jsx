8f28c25102a87b1c43a8fd522044325e138adcce
Rafael Peralta <124094001+Rafaelperalta01@users.noreply.github.com>
2023-11-18 16:47:02 -0300

    Navbar responsive

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -1,21 +1,36 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../images/logo.png';
 import '../styles/nav.css';
+import menuIcon from '../images/menu.png'
+
 
 export default function Nav() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const cerrarNav = () => {
+    setMenuOpen(!menuOpen)
+  }
+
+
   return (
-    <div className='nav'>
-      <img className='logo' src={logo} alt="" />
+    <div className={`nav ${menuOpen ? 'menu-open' : ''}`}>
+      <img className='logo' src={logo} alt="Logo" />
       <div className='links'>
-        <ul>
-          <a href="#main1" className='f'><li>Inicio</li></a>
-          <a href="#servs1" className='f'><li>Programa</li></a>
-          <a href="#333" className='f'><li>Eleginos</li></a>
-          <a href="#listadoprecios" className='f'><li>Precios</li></a>
-          <a href="#main5" className='f'><li>Coaches</li></a>
+        <ul className={`menu-links ${menuOpen ? 'open' : ''}`}>
+          <a href="#main1" className='f'><li onClick={cerrarNav}>Inicio</li></a>
+          <a href="#servs1" className='f'><li onClick={cerrarNav}>Programa</li></a>
+          <a href="#333" className='f'><li onClick={cerrarNav}>Eleginos</li></a>
+          <a href="#listadoprecios" className='f'><li onClick={cerrarNav}>Precios</li></a>
+          <a href="#main5" className='f'><li onClick={cerrarNav}>Coaches</li></a>
         </ul>
       </div>
+      <img className='menu' src={menuIcon} alt="Menu" onClick={toggleMenu} />
     </div>
   );
 }
