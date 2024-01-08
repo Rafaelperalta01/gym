a180f97f6ea575df2685c3f398c6d30c282541e0
Rafael Peralta <124094001+Rafaelperalta01@users.noreply.github.com>
2024-01-08 20:48:28 -0300

    galeria y testimonios aos

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -11,16 +11,16 @@ const Footer = () => {
                         </a>
                         <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                             <li>
-                                <a href="#" class="hover:underline me-4 md:me-6">About</a>
+                                <a href="https://spartansgym.netlify.app" class="hover:underline me-4 md:me-6">About</a>
                             </li>
                             <li>
-                                <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+                                <a href="https://spartansgym.netlify.app" class="hover:underline me-4 md:me-6">Privacy Policy</a>
                             </li>
                             <li>
-                                <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+                                <a href="https://spartansgym.netlify.app" class="hover:underline me-4 md:me-6">Licensing</a>
                             </li>
                             <li>
-                                <a href="#" class="hover:underline">Contact</a>
+                                <a href="https://spartansgym.netlify.app" class="hover:underline">Contact</a>
                             </li>
                         </ul>
                     </div>
